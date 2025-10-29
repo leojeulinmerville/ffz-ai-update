1dc2a7062a38345e05085b92526930d1e48a2aea
leomerville <leojeulinmerville@icloud.com>
2025-10-29 19:31:50 +0100

    Update database.py

--- app/db/database.py
+++ app/db/database.py
@@ -1,13 +1,12 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+from app.config import DATABASE_URL
 
-DATABASE_URL = "sqlite+aiosqlite:///./ffz.db"  # pour local dev
-
-engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+engine = create_async_engine(DATABASE_URL, echo=False, future=True)
 AsyncSessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 Base = declarative_base()
 
 async def init_db():
-    import app.db.models  # importe les modèles
+    import app.db.models  # noqa
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
